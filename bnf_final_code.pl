284b1b7249cd96300893aa460e3a3392f2a28438
EmMongeHnz <emmonge@estudiantec.cr>
2024-09-26 02:00:15 -0600

    Primera persona funcional

--- bnf_final_code.pl
+++ bnf_final_code.pl
@@ -1,41 +1,80 @@
-% Tipos de oraciones
+% Hecho auxiliar para buscar palabras específicas
+es_palabra(Palabra, [Palabra|Resto], Resto).
 
-% Oracion que se divide en sujeto y predicado
-oracion(Completa,Resto):- sujeto(Completa,Interm,_,Cantidad), predicado(Interm,Resto,_,Cantidad).
+% Funcion para verificar una oracion
+verifica(Oracion):- oracion(Oracion,[]).
 
-% Sujeto se compone de un articulo y un sustantivo (genero incluido)
-sujeto(Completa,Resto,Genero,Cantidad):- articulo(Completa,Interm,Genero,Cantidad),sustantivo(Interm,Interm1,Genero,Cantidad),adjetivo(Interm1,Resto,Genero,Cantidad).
+% Estructuras de oraciones
+% Oracion que se divide en un pronombre y un predicado
+oracion(Completa,Resto):- persona(Completa,Interm,_,Cantidad,Pronombre),predicado(Interm,Resto,_,Cantidad,Pronombre),!.
 
-% Caso 1
-% Solamente se especifica accion sin alguien o algo que complemente la oracion
-predicado(Completa,Resto,_,Cantidad):- verbo(Completa,Resto,Cantidad).
+% Oracion que se divide en un me y un predicado condicional (ej: me gusta/gustaria...)
+oracion(Completa,Resto):- es_palabra(me,Completa,Interm),predicado(Interm,Resto,_,_,me),!.
 
-% Caso 2
-% Se especifica un sujeto complementario al verbo del predicado
-predicado(Completa,Resto,Genero,Cantidad):- verbo(Completa,Interm,Cantidad), sujeto(Interm,Resto,Genero,Cantidad).
+% Oracion con pronombre (yo) implicito
+oracion(Completa,Resto):- predicado(Completa,Resto,_,singular,yo),!.
+
+% Oraciones con saludo simple
+oracion(Completa,Resto):- saludo(Completa,Interm), es_palabra(nutritec,Interm,Resto),!.
+
+% Estructuras de predicados
+% Predicados de un sólo verbo independiente
+predicado(Completa,Resto,_,Cantidad,Pronombre):- verbo(Completa,Resto,Cantidad,Pronombre,indep,_),!.
+
+% Predicados de verbos dependientes
+% (hacer y sinonimos) - actividad
+predicado(Completa,Resto,_,Cantidad,Pronombre):- verbo(Completa,Interm,Cantidad,Pronombre,dep,hacer), actividad(Interm,Resto,_),!.
+
+% (tener y sinonimos) - condicion
+predicado(Completa,Resto,_,Cantidad,Pronombre):- verbo(Completa,Interm,Cantidad,Pronombre,dep,tener), condicion(Interm,Resto,_),!.
+
+% (estar) - con - condicion
+predicado(Completa,Resto,_,Cantidad,Pronombre):- verbo(Completa,Interm,Cantidad,Pronombre,dep,estar), es_palabra(con, Interm, Interm1), condicion(Interm1,Resto,_),!.
+
+% gustaria - articulo - sustantivo
+predicado(Completa,Resto,_,Cantidad,_):- verbo(Completa,Interm,Cantidad,me,superdep,agradarFuturo),articulo(Interm,Interm1,Genero,Cantidad,np,indef),sustantivo(Interm1,Resto,Genero,Cantidad,np),!.
+
 
 % Palabras conocidas
 % Articulos:
-% ([Articulo, Resto], Resto, Genero, Cantidad)
-articulo([el|Resto],Resto,masculino,singular).
-articulo([la|Resto],Resto,femenino,singular).
-articulo([los|Resto],Resto,masculino,plural).
-articulo([las|Resto],Resto,femenino,plural).
+% ([Articulo|Resto],Resto,Genero(masculino/femenino),Cantidad(plural/singular),Pronombre(np),Definicion(def/indef))
+articulo([un|Resto],Resto,masculino,singular,np,indef).
+articulo([una|Resto],Resto,femenino,singular,np,indef).
 
-% ([Sustantivo, Resto], Resto, Genero, Cantidad)
-sustantivo([hombre|Resto],Resto,masculino,singular).
-sustantivo([manzana|Resto],Resto,femenino,singular).
-sustantivo([hombres|Resto],Resto,masculino,plural).
-sustantivo([manzanas|Resto],Resto,femenino,plural).
+% Sustantivos
+% ([Sustantivo|Resto],Resto,Genero(masculino/femenino),Cantidad(plural/singular),Pronombre(np))
+sustantivo([ejercicio, Resto],Resto, masculino,singular,np).
 
-% ([Adjetivo, Resto], Resto, Genero, Cantidad)
-adjetivo([fuertes|Resto],Resto,masculino,plural).
-adjetivo([fuertes|Resto],Resto,femenino,plural).
-adjetivo([fuerte|Resto],Resto,masculino,singular).
-adjetivo([fuerte|Resto],Resto,femenino,singular).
-adjetivo([rojas|Resto],Resto,femenino,plural).
-adjetivo([roja|Resto],Resto,femenino,singular).
+% Persona
+% ([Pronombre|Resto],Resto,Genero(masculino/femenino),Cantidad(plural/singular),Pronombre)
+persona([yo|Resto],Resto,masculino,singular,yo).
+persona([me|Resto],Resto,masculino,singular,me).
 
-% ([Verbo, Resto], Resto, Cantidad)
-verbo([come|Resto],Resto,singular).
-verbo([comen|Resto],Resto,plural).
+% Adjetivos
+% ([Adjetivo|Resto],Resto,Genero(masculino/femenino),Cantidad(plural/singular),Pronombre(np))
+adjetivo([intenso|Resto],Resto,masculino,singular,np).
+
+% Verbos
+% ([Verbo|Resto],Resto,Cantidad(plural/singular),Pronombre,Dependencia,Familia)
+verbo([hago|Resto],Resto,singular,yo,dep,hacer).
+verbo([practico|Resto],Resto,singular,yo,dep,hacer).
+verbo([corro|Resto],Resto,singular,yo,indep,accionPresente).
+verbo([entreno|Resto],Resto,singular,yo,indep,accionPresente).
+verbo([tengo|Resto],Resto,singular,yo,dep,tener).
+verbo([estoy|Resto],Resto,singular,yo,dep,estar).
+verbo([gustaria|Resto],Resto,singular,me,superdep,agradarFuturo).
+
+% Actividades
+% ([Actividad|Resto],Resto,Genero)
+actividad([atletismo|Resto],Resto,masculino).
+actividad([ejercicio|Resto],Resto,masculino).
+
+% Condiciones
+% ([Condicion|Resto],Resto,Genero)
+condicion([diabetes|Resto],Resto,femenino).
+
+% Saludo
+% ([Saludo|Resto],Resto)
+saludo([hola|Resto],Resto).
+saludo([saludos|Resto],Resto).
+
