3484ced7e8cc871a46319cf58c2690948cfd8744
MrGabo06 <ggabriel0635@gmail.com>
2024-09-26 01:40:46 -0600

    [ADD]: mejora en el parcer

--- interaction.pl
+++ interaction.pl
@@ -1,13 +1,39 @@
+:- dynamic lista_coincidencias/1.  % Declarar como dinámica
 :- consult('database.pl').  % Cargar la base de datos.
 
 lista_animales([perro, leon, gato, pez]).
+emociones([felicidad, tristeza, ira, sorpresa, miedo]).
+colores([rojo, azul, verde, amarillo, negro]).
+
+% Inicializar la lista de coincidencias vacía
+lista_coincidencias([]).
 
 % inicio del programa
 inicio :-
     write('Bienvenido a Nutritec.'), nl,
     read_line_to_string(user_input, Mensaje),
-    lista_animales(Lista),  % Obtener la lista de animales
-    verificar_palabra(Mensaje, Lista, Palabra).
+    verifica_coincidencias(Mensaje).
+
+verifica_coincidencias(Mensaje) :-
+    ( verifica_padecimiento(Mensaje) ; 
+      verifica_calorias(Mensaje) ; 
+      verifica_dieta(Mensaje) ),
+    imprimir_lista_coincidencias.
+
+verifica_dieta(Mensaje) :-
+    lista_animales(Lista),
+    verificar_palabra(Mensaje, Lista, Palabra),
+    agregar_a_lista(Palabra).
+
+verifica_padecimiento(Mensaje) :-
+    emociones(Lista),
+    verificar_palabra(Mensaje, Lista, Palabra),
+    agregar_a_lista(Palabra).
+
+verifica_calorias(Mensaje) :-
+    colores(Lista),
+    verificar_palabra(Mensaje, Lista, Palabra),
+    agregar_a_lista(Palabra).
 
 % Verificar si alguna palabra de la oración está en la lista predefinida
 verificar_palabra(Oracion, ListaPredefinida, PalabraEncontrada) :-
@@ -30,9 +56,18 @@ miembro_de_lista(Elemento, [Elemento|_]).
 miembro_de_lista(Elemento, [_|Resto]) :-
     miembro_de_lista(Elemento, Resto).
 
-% Sugerir dieta basada en el padecimiento y el nivel de actividad.
-sugerir_dieta(Padecimiento, NivelActividad) :-
-    sugerir_dieta_por_padecimiento(Padecimiento, DietaPorPadecimiento),
-    sugerir_dieta_por_actividad(NivelActividad, DietaPorActividad),
-    write('Se recomienda la dieta: '), write(DietaPorPadecimiento), nl,
-    write('Se recomienda la dieta: '), write(DietaPorActividad), nl.
+% Agregar una palabra a la lista de coincidencias
+agregar_a_lista(Palabra) :-
+    lista_coincidencias(Lista),
+    \+ miembro_de_lista(Palabra, Lista),  % Solo agregar si no está ya en la lista
+    retract(lista_coincidencias(Lista)),  % Eliminar la lista anterior
+    assertz(lista_coincidencias([Palabra | Lista])).  % Agregar la nueva palabra
+
+% Imprimir la lista de coincidencias
+imprimir_lista_coincidencias :-
+    lista_coincidencias(Lista),
+    ( Lista = [] -> 
+        writeln('No se encontraron coincidencias.')
+    ; 
+        write('Palabras encontradas: '), write(Lista), nl  % Cambiado a write para imprimir la lista completa
+    ).
